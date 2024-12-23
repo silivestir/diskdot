e154dc6b3f6ba5a795d50f4f6c92ff17395d77db
silivestir <84616833+silivestir@users.noreply.github.com>
2024-12-23 10:21:24 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -359,7 +359,7 @@ app.use((err, req, res, next) => {
 
 // Start the server
 // Start the server
-const PORT = process.env.PORT || 10000;
+const PORT =10000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
